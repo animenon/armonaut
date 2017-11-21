234cbf62b3f6e38f98c4daee2b07fd9fb3607b61
Seth M. Larson <SethMichaelLarson@users.noreply.github.com>
2017-11-21 09:18:39 -0600

    Add armonaut.yml fields to build and job

--- armonaut/models.py
+++ armonaut/models.py
@@ -1,7 +1,9 @@
 from armonaut import BaseModel
+import base64
 import datetime
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean, SmallInteger
 from sqlalchemy.orm import relationship
+import typing
 
 
 class Project(BaseModel):
@@ -12,6 +14,7 @@ class Project(BaseModel):
 
     default_branch = Column(String, nullable=False, default='master')
     private = Column(Boolean, nullable=False)
+    secret_env = Column(String, default=None)
 
     builds = relationship('Build', back_populates='project')
 
@@ -23,16 +26,26 @@ class Build(BaseModel):
                         default=datetime.datetime.utcnow)
     end_time = Column(DateTime, default=None)
 
+    # HEAD Commit
     commit_branch = Column(String, nullable=False)
     commit_sha = Column(String, nullable=False)
     commit_author = Column(String, nullable=False)
     commit_url = Column(String, nullable=False)
 
+    # Pull Request
     pull_request_number = Column(Integer, default=None)
     pull_request_branch = Column(String, default=None)
     pull_request_slug = Column(String, default=None)
     pull_request_url = Column(String, default=None)
 
+    # armonaut.yml
+    env = Column(String, default=None)
+    install = Column(String, default=None)
+    script = Column(String, default=None)
+    after_success = Column(String, default=None)
+    after_failure = Column(String, default=None)
+    after_script = Column(String, default=None)
+    
     project = relationship('Project', back_populates='builds')
     project_id = Column(Integer, ForeignKey('projects.id'), nullable=False)
     jobs = relationship('Job', back_populates='build')
@@ -50,6 +63,9 @@ class Job(BaseModel):
 
     start_time = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
     end_time = Column(DateTime, default=None)
+    
+    env = Column(String, default=None)
+    container_units = Column(SmallInteger, default=1, nullable=False)
 
     build = relationship('Build', back_populates='jobs')
     build_id = Column(Integer, ForeignKey('builds.id'), nullable=False)
@@ -60,3 +76,31 @@ class Job(BaseModel):
         if end_time is None:
             end_time = datetime.datetime.utcnow()
         return (end_time - self.start_time).total_seconds()
+    
+    def resolve_env(self) -> typing.Dict[str, str]:
+        """Resolves all values for this job's environment variables
+        from all sources. Also includes the project-level `secret_env`
+        if the job has not been triggered by a pull request.
+        """
+        env = {}
+        for k, v in unpack_string_dict(self.build.env).items():
+            env[k] = v
+        for k, v in unpack_string_dict(self.env).items():
+            env[k] = v
+        if self.build.pull_request_number is None:
+            for k, v in unpack_string_dict(self.build.project.secret_env).items():
+                env[k] = v
+        return env
+
+def unpack_string_list(value: str) -> typing.List[str]:
+    """Unpacks a list of strings that has been packed as
+    a NUL-separated list and then base64-encoded
+    """
+    if value is None or value == '':
+        return []
+    return [x.decode('utf-8') for x in base64.b64decode(value.encode('ascii')).split(b'\x00')]
+
+
+def unpack_string_dict(value: str) -> typing.Dict[str, str]:
+    dct = {}
+    lst = unpack_tring_list(value)
