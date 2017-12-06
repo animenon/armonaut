3d5badb47233d1263ef6ba1758a814f517c5a088
lars4839 <sethmichaellarson@protonmail.com>
2017-12-05 23:06:58 -0600

    Add a lot to template

--- armonaut/index/controllers.py
+++ armonaut/index/controllers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_from_directory
 
 
 index = Blueprint('index', __name__, url_prefix='/')
@@ -7,3 +7,8 @@ index = Blueprint('index', __name__, url_prefix='/')
 @index.route('/', methods=['GET'])
 def home():
     return render_template('parent.html')
+
+
+@index.route('/static/<path:path>')
+def static(path):
+    return send_from_directory('static', path)
